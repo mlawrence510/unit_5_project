6b9d5729a8d5e27a4a37d159d2b71ee1e655bac2
Matthew Lawrence <mlawrence8@gmail.com>
2020-07-12 17:22:36 -0400

    cleaned up files finished some loose ends

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 document.getElementById('inputField').addEventListener("keyup", searchImg);
 
 function searchImg() {
-
   const input = document.getElementById('inputField').value.toUpperCase();
   const imgs = document.querySelectorAll('#image-gallery a');
 
-
   for (i = 0; i < imgs.length; i ++){
+
     const imgCaption = document.getElementsByTagName('a')[i].getAttribute('data-title').toUpperCase();
     const indexSearch = imgCaption.indexOf(input);
+
     if(indexSearch > -1) {
       $(document.getElementsByTagName('a')[i]).fadeIn();
     } else {
